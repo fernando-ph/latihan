e86ddcfab156f154c8e2b10df56acec708fdb4ac
fernando-ph <fernandolumbangaol11@gmail.com>
2023-09-04 09:42:30 +0700

    sjaks

--- main.go
+++ main.go
@@ -15,5 +15,30 @@ func main() {
 	var age int
 	age = 345
 	fmt.Println(age)
+	//deklarasi var dengan titik dua
+	//titik dua itu bisa dideklarasi kan tanpa tipe data dengan contoh dibawah ini
+	//tapi tidak bisa dideklarasikan 2 kali dan juga menganti tipe datanya dengan lain cth dibawah ini
+	//nando := "ss"
+	//nando :="ss2"
+	//ini tidk bisa
+	//dan satu lagi tidak bisa ganti tipe data
+	//nando := 2
+	//nando :="s"
+	//tidak bisa tidak bisa ganti tipe
+	mobile := "mobile"
+	fmt.Println(mobile)
+	mobile = "atu"
+	fmt.Println(mobile)
+	mobile2 := 23
+	fmt.Println(mobile2)
+	//bisa mendeklarasikan variable dengan memasukan nya ke dalam kurung
+	//harus bisa dibuat dnegan tipe data yang sama
+	//harus bisa dipakai
+	var (
+		namaDepan     = "s"
+		namaBelakakng = "b"
+	)
+	fmt.Println(namaDepan)
+	fmt.Println(namaBelakakng)
 
 }
